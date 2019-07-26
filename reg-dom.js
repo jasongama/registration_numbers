3dcf35f2ab6aad30c165e64c033d6196879d19d4
jasongama <jasongama967@gmail.com>
2019-07-26 17:51:40 +0200

    done

---
+++ reg-dom.js
@@ -0,0 +1,13 @@
+
+function regElement() {
+var regNumber= document.querySelector(".regName").value
+
+let li = document.createElement('li')
+li.innerHTML=regNumber;
+var newli = document.querySelector(".numberPlates");
+
+newli.appendChild(li);
+
+}
+addButton = document.querySelector("#addButton")
+addButton.addEventListener("click", regElement)
